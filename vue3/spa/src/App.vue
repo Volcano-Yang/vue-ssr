51ecbbda574325c1c590d875d0019637257fed20
Volcano-Yang <648941183@qq.com>
2021-12-27 23:29:55 +0800

    ✨ feat(vue3-spa): add vue-router4

--- vue3/spa/src/App.vue
+++ vue3/spa/src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
-    <bar msg="home bar"></bar>
-    <content msg="home content"></content>
-    <footer-component msg="home footer"></footer-component>
+    <h1>Hello App!</h1>
+    <p>
+      <!--使用 router-link 组件进行导航 -->
+      <!--通过传递 `to` 来指定链接 -->
+      <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+      <router-link to="/">Go to Home</router-link>
+      <router-link to="/topic">Go to Topic</router-link>
+    </p>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script setup lang="ts">
-import Bar from "./components/Bar.vue";
-import FooterComponent from "./components/Footer.vue";
-import Content from "./components/Content.vue";
 </script>
 
 <style>
