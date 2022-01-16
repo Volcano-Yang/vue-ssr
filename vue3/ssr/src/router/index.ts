7a3d61ca1eeb0aa5773ee487b36c0fdbc8486ce0
Volcano-Yang <648941183@qq.com>
2022-01-16 10:51:55 +0800

    fix: 解决打包时的@vue/server-renderer类型报错

--- vue3/ssr/src/router/index.ts
+++ vue3/ssr/src/router/index.ts
@@ -4,25 +4,45 @@ import {
   createWebHistory,
 } from 'vue-router';
 
+/**
+* 1. 引入页面组件
+*/
+import Home from "../pages/home.vue";
+import Topic from "../pages/topic.vue";
 
-// @ts-ignore
-const pages = import.meta.glob('../pages/*.vue');
-const routes = Object.keys(pages).map((path) => {
-  const name = path.match(/\.\.\/pages\/(.*)\.vue$/)[1].toLowerCase();
-  const routePath = `/${name}`;
-  if (routePath === '/home') {
-    return {
-      path: '/',
-      name,
-      component: pages[path],
-    };
-  }
-  return {
-    path: routePath,
-    name,
-    component: pages[path],
-  };
-});
+/**
+*  2. 定义路由
+* 每个路由应该映射一个组件。 其中"component" 可以是
+* 通过 Vue.extend() 创建的组件构造器，
+* 或者，只是一个组件配置对象。
+* 我们晚点再讨论嵌套路由。
+*/
+
+const routes = [
+ { path: "/", component: Home },
+ { path: "/topic", component: Topic },
+];
+
+
+
+// // @ts-ignore
+// const pages = import.meta.glob('../pages/*.vue');
+// const routes = Object.keys(pages).map((path) => {
+//   const name = path.match(/\.\.\/pages\/(.*)\.vue$/)[1].toLowerCase();
+//   const routePath = `/${name}`;
+//   if (routePath === '/home') {
+//     return {
+//       path: '/',
+//       name,
+//       component: pages[path],
+//     };
+//   }
+//   return {
+//     path: routePath,
+//     name,
+//     component: pages[path],
+//   };
+// });
 
 
 export function createRouter() {
